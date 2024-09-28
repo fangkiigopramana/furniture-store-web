8d7ece44746ff91e778ca5a1fcc2579f2c46d6c5
fangkiigopramana <fangkigo13@gmail.com>
2024-09-28 15:28:40 +0700

    fix: total amount data type in orders table

--- database/migrations/2024_09_27_090150_create_orders_table.php
+++ database/migrations/2024_09_27_090150_create_orders_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamp('order_date');
-            $table->decimal('total_amount');
+            $table->decimal('total_amount',10,2);
             $table->timestamps();
         });
     }
